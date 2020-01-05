9d485b08af64dba7a4a8bd641279d57f17076104
shihaojie <517925985@qq.com>
2020-01-05 18:53:54 +0800

    26

--- src/com/shj/leecode/Solution26.java
+++ src/com/shj/leecode/Solution26.java
@@ -1,4 +1,77 @@
 package com.shj.leecode;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+/**
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ *
+ * 给定数组 nums = [1,1,2],
+ *
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ *
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ *
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 说明:
+ *
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ *
+ * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ *
+ * 你可以想象内部操作如下:
+ *
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ *
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Solution26 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int length = new Solution26().removeDuplicates(nums);
+        System.out.println(String.format("函数应该返回新的长度 %d, 并且原数组 nums 的前%d个元素被修改为%s。 ",length,length, Arrays.toString(nums)));
+    }
+
+    /**
+     * [0,0,1,1,1,2,2,3,3,4],
+     * [0,1,0,1,1,2,2,3,3,4],
+     * [0,1,0,1,1,2,2,3,3,4],
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if(nums.length<=1){
+            return nums.length;
+        }
+        int temp;
+        int i = 0;
+        for (int j = 0; j < nums.length;j++) {
+            if(nums[i] < nums[j]) {
+//                temp = nums [i+1];
+//                nums[i+1] = nums[j];
+//                nums[j] = temp;
+                nums[i+1] = nums[j];
+//                System.out.println(Arrays.toString(nums));
+                i++;
+            }
+        }
+        return i+1;
+    }
 }
